108f3bf195a5d10f8268577b07ff7d51496f1218
devKyoun <gkehdrbs36@naver.com>
2023-11-13 10:59:56 +0900

    코드 및 파일 정리, FlipController 추가
    강의 들으면서 내 방식으로 더 효율적인 코드로 수정

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,36 +5,91 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    //Rigidbody2D를 가져오기 담을 용기를 준비하는 것
-    //Unity Inspector에서 연결, 혹은 코드로 연결
-    //public Rigidbody2D rb = this.GetComponent<Rigidbody2D>(); 
     private Rigidbody2D rb;
+
 
     //Control Speed
     [SerializeField]private float moveSpeed;
     [SerializeField]private float jumpForce;
-    private float xInput;
+    private bool doJump;
 
-    // Start is called before the first frame update
+    //Input Value
+    private float xInput;
+    private int facingDir = 1;
+    private bool facingRight = true;
+
+    //Animator Control
+    private Animator anim;
+ 
+
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Movement
+        //입력 감지
+        CheckInput();
+        //애니메이션
+        AnimatorControllers();
+        FlipController();
+    }
 
-
-        xInput = Input.GetAxisRaw("Horizontal");
-
-        // y관한 속도 그대로 ( Nothing Change )
+    void FixedUpdate()
+    {
+        //좌,우 이동
+        Movement();
+        //점프
+        Jump();
+    }
+    private void Movement()
+    {
         rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
 
-        if(Input.GetButtonDown("Jump"))
+    private void CheckInput()
+    {
+        xInput = UnityEngine.Input.GetAxisRaw("Horizontal");
+
+        //Press Jump Button
+        if (UnityEngine.Input.GetButtonDown("Jump"))
         {
+            doJump = true;
+        }
+    }
+
+    private void Jump()
+    {
+        if (doJump)
+        {
+            doJump = false;
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+    }
+
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.velocity.x != 0;
+        anim.SetBool("isMoving", isMoving);
+    }
+
+    private void Flip()
+    {
+        //Switcher
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController()
+    {
+        if(rb.velocity.x > 0 && !facingRight)
+            Flip();
+        else if(rb.velocity.x < 0 && facingRight)
+            Flip();
     }
 }
