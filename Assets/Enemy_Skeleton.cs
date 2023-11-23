1e0544e040ea0dff732d94dee70f0103db962948
devKyoun <gkehdrbs36@naver.com>
2023-11-23 19:08:20 +0900

    스켈레톤에게 Player 감지 기능 구현
    RaycastHit2D 자료형을 이용

--- Assets/Enemy_Skeleton.cs
+++ Assets/Enemy_Skeleton.cs
@@ -8,24 +8,74 @@ public class NewBehaviourScript : Entity
     [Header("Move Info")]
     [SerializeField] private float moveSpeed;
 
+    [Header("Player Detection")]
+    [SerializeField] private float playerCheckDistance;
+    [SerializeField] private LayerMask whatIsPlayer;
+    private RaycastHit2D isPlayerDetected;
+
+    private bool isAttacking;
 
     protected override void Start()
     {
         base.Start();
-        facingDir = 1;
-        facingRight = true;
+        facingDir = -1;
+        facingRight = false;
+        isAttacking = false;
     }
 
     protected override void Update()
     {
         base.Update();
 
+        //플레이어 감지했을때
+        if (isPlayerDetected)
+        {
+            if (isPlayerDetected.distance > 1)
+            {
+                //거리가 1 이상이면 걷기
+                rb.velocity = new Vector2(moveSpeed * 2.0f * facingDir, rb.velocity.y);
+
+                Debug.Log("I see the player");
+                isAttacking = false;
+            }
+            else
+            {
+                Debug.Log("ATTACK!" + isPlayerDetected);
+                isAttacking = true;
+            }
+        }
+        else
+            Movement();
+
         //바닥이 없을때 Flip
-        if (!isGrounded || isWallDetected )
+        if (!isGrounded || isWallDetected)
         {
             Flip();
         }
 
-        rb.velocity = new Vector2(moveSpeed * facingDir, rb.velocity.y);
+    }
+
+    private void Movement()
+    {
+
+         //평시 상태
+         rb.velocity = new Vector2(moveSpeed * facingDir, rb.velocity.y);
+    
+    }
+
+
+    protected override void CollisionChecks()
+    {
+        base.CollisionChecks();
+
+        isPlayerDetected = Physics2D.Raycast(transform.position, Vector2.right, playerCheckDistance * facingDir, whatIsPlayer);
+    }
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + playerCheckDistance * facingDir, transform.position.y));
+
     }
 }
